d6b9fbe13dc978840a60d09495e0f2873d4ea0f0
Martin Yrjölä <martin.yrjola@gmail.com>
2021-12-06 13:15:17 +0200

    Style Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,11 +27,18 @@ const Header = () => {
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  padding: 18px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  align-items: baseline;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  flex: 1;
+  display: flex;
+  gap: 48px;
+  justify-content: center;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
